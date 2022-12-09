7da904796c10c0bb0acfb0118702083f15e19a85
MiliSchoo <milagros.schoo@gmail.com>
2022-12-08 22:20:23 -0300

    base de datos conectada al front !!!!!!! (no agarra postman)

---
+++ schoomilagros/src/main/java/com/portfolio/schoomilagros/SchoomilagrosApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.schoomilagros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoomilagrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoomilagrosApplication.class, args);
+	}
+
+}
---
+++ schoomilagros/src/test/java/com/portfolio/schoomilagros/SchoomilagrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.schoomilagros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoomilagrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
